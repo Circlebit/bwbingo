7dd0a608ebc077c59533de8f6a9f6541b110f3ba
Circlebit <paul.seibel@gmail.com>
2015-04-16 14:45:37 +0200

    delete buttons work now

--- app/views.py
+++ app/views.py
@@ -13,9 +13,13 @@ def newBingo():
 	if request.method == 'POST': # and form.validate(): # add validation!
 		if form.bwInput.data not in bwList and form.bwInput.data != '': # dont accept duplicates or empty string
 			bwList.append(form.bwInput.data)
-			delform.delId.data = len(bwList)-1
 			delButtonList.append(delform)
-	
+
+		delform_answer = DelButtonForm(request.form) #??
+
+		if bwList and delButtonList and delform_answer.delId.data:
+			del bwList[int(delform_answer.delId.data)]
+
 	return render_template('index.html',
 							form = form,
 							bwList = bwList,
