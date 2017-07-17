142e8a89814f5f761943ed581efb67c6feb7744b
Circlebit <paul.seibel@gmail.com>
2017-07-17 16:55:13 +0200

    data stored in URL. Permalinks!

--- README.md
+++ README.md
@@ -7,9 +7,10 @@ trying to write a buzzword bingo webapp with flask.
 ## Todo
  * ~~delete buzzwords from the list~~
  * ~~after adding a buzzword with pressing enter you should be able to type in the next one right away (without clicking on the text field)~~
- * permalinks to bwLists
+ * ~~permalinks to bwLists~~
+ ** this is being implemented by storing the complete list of buzzwords in the URL which is cool because just with the link you've got everything you'll ever need. However there might be limits to what server and browser accept in terms of URL length! [relevant info](https://stackoverflow.com/questions/417142/what-is-the-maximum-length-of-a-url-in-different-browsers)
  * ~~make a bingo table from the bwList~~
  * interactive bingo table for playing in the browser
  * nice printing for playing on paper
  * Some CSS and possibly jQuery for eye candy
- * 
+ *
--- app/views.py
+++ app/views.py
@@ -10,7 +10,7 @@ delButtonList = []
 def newBingo():
 	form = BuzzwordForm()
 	delform = DelButtonForm()
-	
+
 	if request.method == 'POST': # and form.validate(): # add validation!
 		if form.bwInput.data not in bwList and form.bwInput.data != '': # dont accept duplicates or empty string
 			bwList.append(form.bwInput.data)
@@ -21,22 +21,42 @@ def newBingo():
 		if bwList and delButtonList and delform_answer.delId.data:
 			del bwList[int(delform_answer.delId.data)]
 
+	bwList_free = bwList[:]
+	bwList_free.insert(12, "FREE")
+
+	tablelink = request.base_url + "table?shuffle=true&bw=" + "|".join(bwList_free)
+
 	return render_template('index.html',
 							form = form,
 							bwList = bwList,
-							delButtonList = delButtonList)
+							delButtonList = delButtonList,
+							tablelink = tablelink)
 
 
 @app.route('/table', methods=['GET'])
 def table():
 
-	### DEBUG CASE
-	#bwList = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y"]
-	###
-	bwList_shuff = bwList[:]
-	shuffle(bwList_shuff)
+	bwUrl = request.args.get('bw')
+	if bwUrl:
+		bwList = bwUrl.split("|")
 
-	bwList_shuff.insert(12, "FREE")
+	### DEBUG CASES
+	#bwList = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y"]
+	# table?bw=jahvistic|jamnagar|jerseyed|loneness|nonidiomatical|nonplutocratic|beak|changefully|though|unfainting|polytheist|decolorizing|disillusionist|dialogite|scapegoatism|romanticise|scorpion|shopwalker|substitutively|snotty|speediness|stephanotis|stereotyping|sunwise|infancy|hoover|hypnagogic
+
+	bwList_shuff = bwList[:]
+	if request.args.get('shuffle'):
+		del bwList_shuff[12]
+		shuffle(bwList_shuff)
+		bwList_shuff.insert(12, "FREE")
+
+
+	#bwList_shuff.insert(12, "FREE")
+
+	permalink = request.base_url + "?bw=" + "|".join(bwList_shuff)
+	reshuffle = request.base_url + "?shuffle=true&bw=" + "|".join(bwList_shuff)
 
 	return render_template('table.html',
-							bwList_shuff=bwList_shuff)
+							bwList_shuff=bwList_shuff,
+							permalink = permalink,
+							reshuffle = reshuffle)
